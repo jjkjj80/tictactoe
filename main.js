9d3e67e4c603323fd29427571a93be103639dfc7
Jason Jewell <jjkjj80@gmail.com>
2023-11-28 12:34:47 -0500

    create and style play board and query all cells in js

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ const Game = (function () {
     let gameboard = [];
     let players = [];
     let gameFlow = {};
-    let qdom = {}
+
+    //DOM Query
+    let tl = document.querySelector("#tl");
+    let tm = document.querySelector("#tm");
+    let tr = document.querySelector("#tr");
+    let ml = document.querySelector("#ml");
+    let mm = document.querySelector("#mm");
+    let mr = document.querySelector("#mr");
+    let bl = document.querySelector("#bl");
+    let bm = document.querySelector("#bm");
+    let br = document.querySelector("#br");
+
     function Player(name) {
         let playerName = name;
         let score = 0;
@@ -13,6 +24,10 @@ const Game = (function () {
     function addPlayer(name) {
         let newPlayer = Player(name);
         players.push(newPlayer);
+    }
+    function marker() {
+        let turn = 0;
+
     }
     return {players, Player, addPlayer}
 })();
